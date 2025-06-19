bbbccc401373ea3611a936638a6bc2c97c55d736
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2025-06-19 17:57:51 +0100

    Set tval to instruction address on ebreak
    See https://github.com/openhwgroup/cva6/issues/3019

--- core/decoder.sv
+++ core/decoder.sv
@@ -2058,6 +2058,7 @@ module decoder
         // set gva bit
         if (CVA6Cfg.RVH) instruction_o.ex.gva = v_i;
         else instruction_o.ex.gva = 1'b0;
+        if (CVA6Cfg.TvalEn) instruction_o.ex.tval = pc_i;
       end
       // -----------------
       // Interrupt Control
