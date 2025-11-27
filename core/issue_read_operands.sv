3c5c6beabcda8d3e97d331531d539f5c21863c76
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2025-11-27 13:44:42 +0000

    Fix CLU structural hazard detection for superscalar

--- core/issue_read_operands.sv
+++ core/issue_read_operands.sv
@@ -415,6 +415,7 @@ module issue_read_operands
           end else begin
             fus_busy[1].alu = 1'b1;
             fus_busy[1].ctrl_flow = 1'b1;
+            fus_busy[1].clu = 1'b1;
             fus_busy[1].csr = 1'b1;
           end
         end
@@ -432,7 +433,10 @@ module issue_read_operands
           fus_busy[1].store = 1'b1;
         end
         CLU: begin
+          fus_busy[1].alu = 1'b1;
           fus_busy[1].clu = 1'b1;
+          fus_busy[1].ctrl_flow = 1'b1;
+          fus_busy[1].csr = 1'b1;
         end
         CVXIF: ;
         default: ;
