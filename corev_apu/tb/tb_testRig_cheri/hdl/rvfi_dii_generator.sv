9f7ce8bc9ae63eaa90c9556361f020cfdb7bbd7a
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2025-11-27 16:37:04 +0000

    Fix rvfi_dii_generator logic for superscalar fetches
    In particular, the logic to test if we're off the end of a test
    needs to be made much more robust since we can be requesting
    and consuming more things in a cycle

--- corev_apu/tb/tb_testRig_cheri/hdl/rvfi_dii_generator.sv
+++ corev_apu/tb/tb_testRig_cheri/hdl/rvfi_dii_generator.sv
@@ -48,16 +48,18 @@ module rvfi_dii_generator
     logic [CVA6Cfg.VLEN-1:0] vaddr_buff;
 
     logic found_last_id_q, found_last_id_d;
-    logic [CVA6Cfg.DIIIDLEN-1:0] test_last_id_q;
+    logic [CVA6Cfg.DIIIDLEN-1:0] test_last_id_q, test_last_id_d;
 
     always_comb begin
         automatic logic [CVA6Cfg.FETCH_WIDTH*2-1:0] instr = '0; // Wider than needed to be shifted in
         automatic logic [2:0] instr_bytes = '0;
         automatic logic [CVA6Cfg.DIIIDLEN-1:0] last_id_offset = '0;
+        automatic logic past_end = '0;
         dii_id_d = dii_id_q;
         fetch_offset_d = fetch_offset_q;
         fetch_buff_d = fetch_buff_q;
-        found_last_id_d = 1'b0;
+        found_last_id_d = found_last_id_q;
+        test_last_id_d = test_last_id_q;
         if (busy && !dreq_i.kill_s2) begin
             // Assume the pipeline consumed the full buffer
             fetch_offset_d = {1'b0, fetch_offset_d[CVA6Cfg.FETCH_ALIGN_BITS-1:0]};
@@ -66,13 +68,19 @@ module rvfi_dii_generator
             fetch_offset_d = fetch_offset_d + vaddr_buff[CVA6Cfg.FETCH_ALIGN_BITS-1:0];
             fetch_buff_d = fetch_buff_d << {vaddr_buff[CVA6Cfg.FETCH_ALIGN_BITS-1:0], 3'b0};
             while (~fetch_offset_d[CVA6Cfg.FETCH_ALIGN_BITS] & (~fetch_offset_d[CVA6Cfg.FETCH_ALIGN_BITS-1:0] != 0)) begin
-                last_id_offset = dii_id_d - test_last_id_q;
-                found_last_id_d = (found_last_id_q && ~last_id_offset[CVA6Cfg.DIIIDLEN-1]) || get_dii_cmd(dii_id_d) == 0;
-                instr = found_last_id_d ? end_of_test_instr : get_dii_insn(dii_id_d);
+                last_id_offset = dii_id_d - test_last_id_d;
+                past_end = found_last_id_d && ~last_id_offset[CVA6Cfg.DIIIDLEN-1];
+                if (!found_last_id_d && get_dii_cmd(dii_id_d) == 0) begin
+                    found_last_id_d = 1'b1;
+                    test_last_id_d = dii_id_d;
+                end
+                // It seems the call to get_dii_insn can happen even if we're past the end
+                // Make sure the argument is always legal so we don't hang if it gets called spuriously
+                instr = past_end ? end_of_test_instr : get_dii_insn(past_end ? test_last_id_d : dii_id_d);
                 instr_bytes = instr[1:0] == 2'b11 ? 3'd4 : 3'd2;
                 fetch_buff_d = fetch_buff_d | (instr << ({fetch_offset_d, 3'b0}));
                 fetch_offset_d = fetch_offset_d + instr_bytes;
-                if (!found_last_id_d) dii_id_d = dii_id_d + 1;
+                if (!past_end) dii_id_d = dii_id_d + 1;
             end
             dreq_o.valid = 1'b1;
         end else begin
@@ -96,8 +104,8 @@ module rvfi_dii_generator
             found_last_id_q <= '0;
             test_last_id_q <= '0;
         end else begin
-            found_last_id_q <= found_last_id_q | found_last_id_d;
-            if (found_last_id_d) test_last_id_q <= dii_id_d;
+            found_last_id_q <= found_last_id_d;
+            test_last_id_q <= test_last_id_d;
             if (dreq_i.kill_s1 || dreq_i.kill_s2) begin
                 fetch_buff_q <= '0;
                 fetch_offset_q <= '0;
