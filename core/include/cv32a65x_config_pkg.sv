9547c73a70d6e173ed0b2b5e9710b43456d3ac9e
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2025-12-19 01:24:09 +0000

    Fix all configs for CHERI
    Ensure all the flags are present, but set to no-cheri.
    Also standardise the order.

--- core/include/cv32a65x_config_pkg.sv
+++ core/include/cv32a65x_config_pkg.sv
@@ -56,6 +56,8 @@ package cva6_config_pkg;
       RVZiCond: bit'(0),
       RVZicntr: bit'(0),
       RVZihpm: bit'(0),
+      RVZcheripurecap: bit'(0),
+      RVZcherihybrid: bit'(0),
       NrScoreboardEntries: unsigned'(8),
       PerfCounterEn: bit'(0),
       MmuPresent: bit'(0),
@@ -116,7 +118,8 @@ package cva6_config_pkg;
       NrStorePipeRegs: int'(0),
       DcacheIdWidth: int'(1),
       CheriCapTagWidth : int'(1),
-      RVFI_DII : int'(0)
+      RVFI_DII : int'(0),
+      DIIIDLEN : int'(0)
   };
 
 endpackage
