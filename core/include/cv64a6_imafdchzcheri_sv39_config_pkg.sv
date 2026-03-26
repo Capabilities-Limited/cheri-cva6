5a0a2ac5aedcd1c0383a173bd15de140420e0e67
Jonathan Woodruff <jdw57@cam.ac.uk>
2026-03-26 09:12:12 +0000

    Back to 8 sets for dcache.

--- core/include/cv64a6_imafdchzcheri_sv39_config_pkg.sv
+++ core/include/cv64a6_imafdchzcheri_sv39_config_pkg.sv
@@ -46,7 +46,7 @@ package cva6_config_pkg;
   localparam CVA6ConfigIcacheSetAssoc = 4;
   localparam CVA6ConfigIcacheLineWidth = 128;
   localparam CVA6ConfigDcacheByteSize = 32768;
-  localparam CVA6ConfigDcacheSetAssoc = 4;
+  localparam CVA6ConfigDcacheSetAssoc = 8;
   localparam CVA6ConfigDcacheLineWidth = 1024;
 
   localparam CVA6ConfigDcacheFlushOnFence = 1'b1;
