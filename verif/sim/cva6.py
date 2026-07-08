d7aca7dcfb0d6623a4a98b3d4456bf4cbdd97508
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2026-07-08 19:02:03 +0100

    Bump verilator version
    This version seems to have more support for covergroups

--- verif/sim/cva6.py
+++ verif/sim/cva6.py
@@ -1035,7 +1035,7 @@ def check_spike_version():
 
 
 def check_verilator_version():
-  REQUIRED_VERILATOR_VERSION = "5.038"
+  REQUIRED_VERILATOR_VERSION = "5.050"
 
   verilator_version_string = run_cmd("verilator --version")
   logging.info(f"Verilator Version: {verilator_version_string.strip()}")
