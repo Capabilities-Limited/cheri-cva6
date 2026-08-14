88c1478b6485a78d4e6ac3d9d004374e4479cebc
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2026-08-14 16:15:14 +0100

    Hook up tagcfg interface in corev_apu
    This is currently untested, but probably better than leaving the
    AXI inputs floating for now

--- corev_apu/fpga/src/ariane_xilinx.sv
+++ corev_apu/fpga/src/ariane_xilinx.sv
@@ -346,6 +346,7 @@ assign rst = ddr_sync_reset;
 axi_pkg::xbar_rule_64_t [ariane_soc::NB_PERIPHERALS-1:0] addr_map;
 
 assign addr_map = '{
+  '{ idx: ariane_soc::TagCfg,   start_addr: ariane_soc::TagCfg,       end_addr: ariane_soc::TagCfg + ariane_soc::TagCfgLength         },
   '{ idx: ariane_soc::Debug,    start_addr: ariane_soc::DebugBase,    end_addr: ariane_soc::DebugBase + ariane_soc::DebugLength       },
   '{ idx: ariane_soc::ROM,      start_addr: ariane_soc::ROMBase,      end_addr: ariane_soc::ROMBase + ariane_soc::ROMLength           },
   '{ idx: ariane_soc::CLINT,    start_addr: ariane_soc::CLINTBase,    end_addr: ariane_soc::CLINTBase + ariane_soc::CLINTLength       },
@@ -1238,12 +1239,16 @@ localparam int unsigned AxiStrbWidth = AxiDataWidth / 32'd8;
 
   ariane_axi_soc::req_slv_t  dram_req;
   ariane_axi_soc::resp_slv_t dram_resp;
-  axi_mst_req_t axi_tag_req;
-  axi_mst_resp_t axi_tag_resp;
   `AXI_ASSIGN_TO_REQ(dram_req, dram)
   `AXI_ASSIGN_FROM_RESP(dram, dram_resp)
+  axi_mst_req_t axi_tag_req;
+  axi_mst_resp_t axi_tag_resp;
   `AXI_ASSIGN_FROM_REQ(tag_mst, axi_tag_req)
   `AXI_ASSIGN_TO_RESP(axi_tag_resp, tag_mst)
+  ariane_axi_soc::req_slv_t tagcfg_req;
+  ariane_axi_soc::resp_slv_t tagcfg_resp;
+  `AXI_ASSIGN_TO_REQ(tagcfg_req, master[ariane_soc::TagCfg])
+  `AXI_ASSIGN_FROM_RESP(master[ariane_soc::TagCfg], tagcfg_resp)
   `REG_BUS_TYPEDEF_ALL(conf, logic [31:0], logic [31:0], logic [3:0])
 
   if (CVA6Cfg.CheriPresent) begin
@@ -1268,8 +1273,8 @@ localparam int unsigned AxiStrbWidth = AxiDataWidth / 32'd8;
         .clk_i          (clk),
         .rst_ni         (ndmreset_n),
         .test_i         (1'b0),
-        .cfg_slv_req_i  (/*TODO*/),
-        .cfg_slv_resp_o (/*TODO*/),
+        .cfg_slv_req_i  (tagcfg_req),
+        .cfg_slv_resp_o (tagcfg_resp),
         .slv_req_i      (dram_req),
         .slv_resp_o     (dram_resp),
         .mst_req_o      (axi_tag_req),
